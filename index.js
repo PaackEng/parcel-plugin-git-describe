8f4a2c6ca8ade28a3e45638286347e3d42c62b9d
PedroHLC <root@pedrohlc.com>
2020-06-02 14:50:18 -0300

    One line only

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const exec = util.promisify(require('child_process').exec);
 module.exports = async function () {
   try {
     const { stdout } = await exec('git describe --always --tags --dirty=+');
-    process.env.GIT_DESCRIBE = stdout;
+    process.env.GIT_DESCRIBE = stdout.toString().split('\n', 1)[0];
   } catch (e) { console.log(e); }
 }
